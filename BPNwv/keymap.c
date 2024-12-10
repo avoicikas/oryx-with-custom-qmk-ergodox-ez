78a607f11e8c4ebd266750f9d820c5c02b0db475
Aleksandras Voicikas <avoicikas@gmail.com>
2024-12-10 22:45:31 +0200

    exclude from acordion thumbs

--- BPNwv/keymap.c
+++ BPNwv/keymap.c
@@ -10,6 +10,30 @@ uint16_t achordion_timeout(uint16_t tap_hold_keycode) {
   return 100;
 }
 
+bool achordion_chord(uint16_t tap_hold_keycode,
+                     keyrecord_t* tap_hold_record,
+                     uint16_t other_keycode,
+                     keyrecord_t* other_record) {
+  // Exceptionally consider the following chords as holds, even though they
+  // are on the same hand
+  // switch (tap_hold_keycode) {
+  //   case HOME_A:  // A + U.
+  //     if (other_keycode == HOME_U) { return true; }
+  //     break;
+  //
+  //   case HOME_S:  // S + H and S + G.
+  //     if (other_keycode == HOME_H || other_keycode == KC_G) { return true; }
+  //     break;
+  // }
+
+  // Also allow same-hand holds when the other key is in the rows below the
+  // alphas. I need the `% (MATRIX_ROWS / 2)` because my keyboard is split.
+  if (other_record->event.key.row % (MATRIX_ROWS / 2) >= 4) { return true; }
+
+  // Otherwise, follow the opposite hands rule.
+  return achordion_opposite_hands(tap_hold_record, other_record);
+}
+
 enum custom_keycodes {
   RGB_SLD = EZ_SAFE_RANGE,
   ST_MACRO_0,
