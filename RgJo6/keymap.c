b7a7c0665c05dd18453ec7ccc2978ebe60456c05
Aleksandras Voicikas <avoicikas@gmail.com>
2024-12-11 14:11:00 +0200

    repeat added again after tap changes

--- RgJo6/keymap.c
+++ RgJo6/keymap.c
@@ -864,7 +864,7 @@ void dance_13_reset(tap_dance_state_t *state, void *user_data) {
 
 tap_dance_action_t tap_dance_actions[] = {
         [DANCE_0] = ACTION_TAP_DANCE_TAP_HOLD(KC_0, LGUI(KC_SPACE)),
-        [DANCE_11] = ACTION_TAP_DANCE_TAP_HOLD(KC_DELETE, KC_A),
+        [DANCE_11] = ACTION_TAP_DANCE_TAP_HOLD(KC_DELETE, QK_REPEAT_KEY),
         [DANCE_14] = ACTION_TAP_DANCE_TAP_HOLD(KC_RIGHT, KC_END),
         [DANCE_1] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_1, dance_1_finished, dance_1_reset),
         [DANCE_2] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_2, dance_2_finished, dance_2_reset),
